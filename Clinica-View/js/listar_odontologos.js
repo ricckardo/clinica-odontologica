b54c15b52f0800c0da9a40aabb36bc420bbf2e53
Raul Garcia <rgarcia@innova360.biz>
2024-06-13 11:19:35 -0600

    Generacion de README

--- Clinica-View/js/listar_odontologos.js
+++ Clinica-View/js/listar_odontologos.js
@@ -1,11 +1,13 @@
 const tableBody = document.querySelector("#odontologosTable tbody");
 const apiURL = "http://localhost:8080";
+
 function fetchOdontologos() {
+  
   // listando los odontologos
 
   fetch(`${apiURL}/odontologos`)
-    .then((response) => response.json())
-    .then((data) => {
+    .then((response) => response.json()) // se decodifica la data de la promesa
+    .then((data) => {                    // se trabaja con la data decodificada en JSON
       console.log(data);
       // Limpiar el contenido actual de la tabla
       tableBody.innerHTML = "";
@@ -13,6 +15,14 @@ function fetchOdontologos() {
       // Insertar los datos en la tabla
       data.forEach((odontologo, index) => {
         const row = document.createElement("tr");
+
+        /**
+               tr: table row (fila de tabla)
+               th: table header (encabezado de tabla)
+               tbody: table body (cuerpo de la tabla)
+               td: table data (dato de tabla o celda de tabla)
+        Estos elementos se utilizan para estructurar y organizar los datos dentro de una tabla en HTML.
+        */
 
         row.innerHTML = `
                 <td>${odontologo.id}</td>
@@ -41,8 +51,16 @@ function fetchOdontologos() {
 
   // modificar un odontologo
 
+  modal.addEventListener("submit", function (event){
+    event.preventDefault();
+
+  });
 
   // eliminar un odontologo
+
+  btnEliminar.addEventListener("submit", function (event) {
+    
+  })
 }
 
 fetchOdontologos();
